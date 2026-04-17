900c05ee2dff229281f0e86083fec7f6a02aaf3a
Mio <gusw.0246.lpsyg@gmail.com>
2026-04-17 20:01:12 +0200

    feature(data.js) created

---
+++ assets/data/data.js
@@ -0,0 +1,66 @@
+export const data = {
+    personalInfo:{
+        nombre: "john",
+        apellidos: "Doe",
+        telefono1:"1234567",
+        telefono2: null,
+        email_profesional1: "john_doe@email.com",
+        email_profesional2: "john_doe2@email.com",
+        ciudad : "Barcelona",
+        pais: "España",
+        codigo_postal : "08020",
+        fecha_nacimiento: "2000-01-01",
+        nacionalidad: "Española",
+        carnet_coducir: true,
+        disponibilidad:"aaaaaa",
+        foto: 'https://pic.com',
+    },
+    professionalProfile:{
+        profesion: "Full Stack Developer",
+        objetivo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+        resumen:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    workExperience:[
+        {
+            puest:"Full Stack Developer",
+            empresa:"aaaaaaaaaa",
+            ubicacion:"Barcelona",
+            fecha_inicio:"2019-02-02",
+            fecha_fin:"2024-03-03",
+            trabajo_actual:false,
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            logros: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            orden :1
+        }
+    ],
+    education:[
+        {
+            titulo: "matematica",
+            centro:"Univerdidad A",
+            ubicacion: "Barcelona",
+            fecha_inicio: "2015-04-04",
+            feha_fin: "2019-01-01",
+            en_curso: true,
+            orden:1
+        }
+    ],
+    complementaryTraining:[
+        {
+            tipo: "aaaaaaa",
+            nombre: "aaaaaaa",
+            entidad: "aaaaaaa",
+            fecha: "2023-05-05",
+            description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            orden : 1
+        }
+    ],
+    skill:
+    {
+    tipo: "aaaa",
+    categoria:"aaaaa",
+    nombre:"bbbbb",
+    nivel:5,
+    orden:1
+    }
+    
+}
